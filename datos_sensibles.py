e234a278ad84896911b612007ec3ff0d414c5902
nachompra <ignacio.mpra@gmail.com>
2025-01-15 22:21:06 +0100

    eda limpio

---
+++ datos_sensibles.py
@@ -0,0 +1,6 @@
+#client_spotify
+CLIENT_ID = '132cda9340494348a081804e1cfdc1c8'
+CLIENT_SECRET = 'c27c89006c53437a83761f0185e465d8'
+#gmail ad.tiktok
+user = 'ancho.research@gmail.com'
+pw = 'scrapper_sel'
